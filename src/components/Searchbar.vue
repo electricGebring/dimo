665342818eb541d50ad2149ce25b8d7e2a207972
Alex Duong <alex.duong@live.se>
2021-03-18 10:35:05 +0100

    autocomplete start

---
+++ src/components/Searchbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="searchbar">
+    <Autocomplete @input="Elements" :result="Elements">{{ Elements.Label }}</Autocomplete>
+  </div>
+</template>
+<script>
+import Autocomplete from 'vue3-autocomplete'
+import 'vue3-autocomplete/dist/vue3-autocomplete.css'
+
+export default {
+  name: 'Searchbar',
+  components: {
+    Autocomplete,
+  },
+  mounted() {
+    this.$store.dispatch('getDocuments')
+  },
+  computed: {
+    Elements() {
+      return this.$store.state.Elements
+    },
+  },
+}
+</script>
+<style scoped>
+.searchbar {
+  margin-left: 20%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,20 @@
   <div id="home">
     <sidebar />
     <Categories />
+    <Searchbar />
   </div>
 </template>
 
 <script>
 import Sidebar from '../components/Sidebar.vue'
 import Categories from '../components/Categories.vue'
+import Searchbar from '../components/Searchbar.vue'
 export default {
   name: 'Home',
   components: {
     Sidebar,
     Categories,
+    Searchbar,
   },
 }
 </script>
-<style lang="scss"></style>
