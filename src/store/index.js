fcef559c34f4a8bf18de5b198555c8144ae11296
electricGebring <56607795+electricGebring@users.noreply.github.com>
2021-04-23 12:10:12 +0200

    har lagt till i store för att kunna använda mot checkboxar

--- src/store/index.js
+++ src/store/index.js
@@ -4,17 +4,24 @@ import axios from 'axios'
 export default createStore({
   state: {
     Elements: [],
+    allCheckboxesSelected: false,
   },
   mutations: {
     GET_INFO(state, Elements) {
       state.Elements = Elements;
-    }
+    },
+    changeCheck(state) {
+      state.allCheckboxesSelected = !state.allCheckboxesSelected;
+    },
   },
   actions: {
     getDocuments({commit}) {
       axios.get('data.json').then((response) => {
         commit('GET_INFO', response.data.Elements)
       })
+    },
+    changeCheckboxes({context}) {
+      context.commit('changeCheck')
     }
   },
   modules: {
