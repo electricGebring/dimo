f05aa1c1fdfc4e7250cae11400a779d739ed7763
electricGebring <56607795+electricGebring@users.noreply.github.com>
2021-04-28 13:58:21 +0200

    logga skit som ska med

--- src/composables/postElements.js
+++ src/composables/postElements.js
@@ -1,7 +1,9 @@
 const postElements = async (skitSomSkaMed) => {
-    await fetch('http://localhost:3000/posts', {
-        method: 'POST',
-        headers: { 'Content-type': application/json },
-        body: JSON.stringify(skitSomSkaMed)
-    })
+    console.log(skitSomSkaMed)
+    
+    // await fetch('http://localhost:3000/posts', {
+    //     method: 'POST',
+    //     headers: { 'Content-type': application/json },
+    //     body: JSON.stringify(skitSomSkaMed)
+    // })
 }
