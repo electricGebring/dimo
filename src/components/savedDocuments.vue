08b402e10b27578195d11f635949841ccd222a31
ingridgrenholm <65853333+ingridgrenholm@users.noreply.github.com>
2021-04-28 13:51:14 +0200

    wip
    wip

--- src/components/savedDocuments.vue
+++ src/components/savedDocuments.vue
@@ -2,21 +2,38 @@
   <div class="savedlist-container">
     <h3>Mina sparade dokument</h3>
     <div class="savedlist-object">
-     <img class="pdf-icon" src="/img/pdficon.svg" alt=""/>
+      <img
+        class="pdf-icon"
+        src="https://i.ibb.co/5M6fZr1/Test-23.png"
+        alt="Test-23"
+        border="0"
+      />
       <h4>Styrdokument nummer # 34</h4>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
         risus odio tincidunt nulla suspendisse tempus
       </p>
     </div>
-     <div class="savedlist-object">
+    <div class="savedlist-object">
+      <img
+        class="pdf-icon"
+        src="https://i.ibb.co/5M6fZr1/Test-23.png"
+        alt="Test-23"
+        border="0"
+      />
       <h4>Styrdokument nummer # 34</h4>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
         risus odio tincidunt nulla suspendisse tempus
       </p>
     </div>
-     <div class="savedlist-object">
+    <div class="savedlist-object">
+      <img
+        class="pdf-icon"
+        src="https://i.ibb.co/5M6fZr1/Test-23.png"
+        alt="Test-23"
+        border="0"
+      />
       <h4>Styrdokument nummer # 34</h4>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
@@ -37,7 +54,7 @@ export default {
   background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
-  width: 30%;
+  width: 20vw;
   padding: 16px 40px 16px 40px;
   margin-top: 30px;
   font-family: Montserrat;
@@ -50,23 +67,29 @@ export default {
     color: #002065;
   }
 
-  .savedlist-object  {
-      margin-top: 20px;
-  h4 {
-    font-style: normal;
-    font-size: 12px;
-    line-height: 16px;
-    color: #2c365a;
-    margin: 0;
-  }
-  p {
-    font-style: normal;
-    font-weight: 300;
-    font-size: 7px;
-    line-height: 9px;
-    color: #000000;
-    margin: 0;
-  }
+  .savedlist-object {
+    margin-top: 20px;
+
+    .pdf-icon {
+      width: 40px;
+      float: left;
+      margin-right: 10px;
     }
+    h4 {
+      font-style: normal;
+      font-size: 12px;
+      line-height: 16px;
+      color: #2c365a;
+      margin: 0;
+    }
+    p {
+      font-style: normal;
+      font-weight: 300;
+      font-size: 7px;
+      line-height: 9px;
+      color: #000000;
+      margin: 0;
+    }
+  }
 }
 </style>
