c0329002780f35cc02c79b427804ac533e866f4b
electricGebring <56607795+electricGebring@users.noreply.github.com>
2021-04-29 09:14:26 +0200

    wip

--- src/composables/postElements.js
+++ src/composables/postElements.js
@@ -1,11 +1,11 @@
 const postElements = async (skitSomSkaMed) => {
     console.log(skitSomSkaMed, 'skitSomSkaMed i postElements')
     
-    // await fetch('http://localhost:3000/posts', {
-    //     method: 'POST',
-    //     headers: { 'Content-type': application/json },
-    //     body: JSON.stringify(skitSomSkaMed)
-    // })
+    await fetch('http://localhost:3001/', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(skitSomSkaMed)
+    })
 }
 
 export default postElements
