1dc9c2ff5c82d27fd54311b4f0bcdf1c25e03f09
electricGebring <56607795+electricGebring@users.noreply.github.com>
2021-05-17 15:12:52 +0200

    ok, modal funkar

--- src/components/Doclist.vue
+++ src/components/Doclist.vue
@@ -51,12 +51,14 @@
             <span>{{ item.Documenttype }} </span>
           </div>
           <div class="icons">
-            <span class=""><img src="/img/view-eye.svg" alt="" /></span>
-            <span class="" v-on:click.stop="handleSave(item._id)">
+            <span @click.stop="$refs.johansModal.openModal()">
+              <img src="/img/view-eye.svg" alt="" />
+            </span>
+            <span v-on:click.stop="handleSave(item._id)">
               <img
                 class="save"
                 src="/img/star.svg"
-                v-bind:class="{ 'saveActive': savedDocumentsCheck(item._id) }"
+                v-bind:class="{ saveActive: savedDocumentsCheck(item._id) }"
                 width="300"
                 height="150"
               />
@@ -66,23 +68,78 @@
       </div>
     </div>
   </div>
+  <modal ref="johansModal">
+    <template v-slot:header>
+      <h1>Modal title</h1>
+    </template>
+
+    <template v-slot:body>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nunc sed velit
+        dignissim sodales ut eu sem integer vitae. Id aliquet lectus proin nibh
+        nisl condimentum. Fringilla urna porttitor rhoncus dolor purus. Nam
+        aliquam sem et tortor. Nisl vel pretium lectus quam id. Cras pulvinar
+        mattis nunc sed. Quis ipsum suspendisse ultrices gravida dictum fusce ut
+        placerat orci. Tristique magna sit amet purus. Fermentum et sollicitudin
+        ac orci phasellus egestas tellus. Erat pellentesque adipiscing commodo
+        elit at imperdiet dui accumsan. Felis eget nunc lobortis mattis aliquam
+        faucibus. Tincidunt eget nullam non nisi est sit amet facilisis. Mi in
+        nulla posuere sollicitudin aliquam ultrices sagittis orci. Vitae proin
+        sagittis nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque
+        viverra mauris in aliquam sem fringilla ut. Nec nam aliquam sem et
+        tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a.
+        Elementum tempus egestas sed sed. Faucibus purus in massa tempor nec
+        feugiat nisl pretium fusce.
+      </p>
+      <p>
+        Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc
+        mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Ut
+        faucibus pulvinar elementum integer enim. Odio facilisis mauris sit amet
+        massa vitae tortor condimentum lacinia. Eu non diam phasellus vestibulum
+        lorem sed risus. Porttitor rhoncus dolor purus non enim praesent. Sit
+        amet mauris commodo quis imperdiet. Lobortis feugiat vivamus at augue
+        eget. Nibh tellus molestie nunc non blandit. Tellus mauris a diam
+        maecenas sed enim ut. Tortor aliquam nulla facilisi cras fermentum odio
+        eu feugiat pretium. Mattis aliquam faucibus purus in massa.
+      </p>
+    </template>
+
+    <template v-slot:footer>
+      <div class="d-flex align-items-center justify-content-between">
+        <button
+          class="btn btn--secondary"
+          @click="$refs.modalName.closeModal()"
+        >
+          Cancel
+        </button>
+        <button class="btn btn--primary" @click="$refs.modalName.closeModal()">
+          Save
+        </button>
+      </div>
+    </template>
+  </modal>
 </template>
 <script>
+import Modal from "./Modal.vue";
+
 export default {
   props: ["doclist"],
   data() {
     return {
       saveActive: false,
       targetArea: [],
-    }
+    };
   },
-  components: {},
+  components: {
+    Modal
+  },
   computed: {
     savedDocuments() {
-      return this.$store.state.savedDocuments
+      return this.$store.state.savedDocuments;
     },
     recentlyViewed() {
-      return this.$store.state.recentlyViewed
+      return this.$store.state.recentlyViewed;
     },
   },
   methods: {
@@ -98,18 +155,18 @@ export default {
     },
     handleSave(id) {
       if (this.savedDocumentsCheck(id)) {
-        this.$store.dispatch("deleteSavedDocuments", id)
+        this.$store.dispatch("deleteSavedDocuments", id);
       } else {
-        this.$store.dispatch("postSavedDocuments", id)
+        this.$store.dispatch("postSavedDocuments", id);
       }
     },
     savedDocumentsCheck(id) {
       for (let i = 0; i < this.savedDocuments.length; i++) {
         if (this.savedDocuments[i]._id === id) {
-          return true
-        }   
+          return true;
+        }
       }
-      return false
+      return false;
     },
   },
 };
