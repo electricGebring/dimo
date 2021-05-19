3429323b9522e11a0ecd3276ffd5b83dcf4d587e
electricGebring <56607795+electricGebring@users.noreply.github.com>
2021-05-19 09:16:16 +0200

    wip

--- src/components/Doclist.vue
+++ src/components/Doclist.vue
@@ -70,7 +70,7 @@
   </div>
   <modal ref="johansModal">
     <template v-slot:header>
-      <h1>Modal title</h1>
+      <h1>Liten beskrivande text om dokumentet</h1>
     </template>
 
     <template v-slot:body>
@@ -82,40 +82,14 @@
         aliquam sem et tortor. Nisl vel pretium lectus quam id. Cras pulvinar
         mattis nunc sed. Quis ipsum suspendisse ultrices gravida dictum fusce ut
         placerat orci. Tristique magna sit amet purus. Fermentum et sollicitudin
-        ac orci phasellus egestas tellus. Erat pellentesque adipiscing commodo
-        elit at imperdiet dui accumsan. Felis eget nunc lobortis mattis aliquam
-        faucibus. Tincidunt eget nullam non nisi est sit amet facilisis. Mi in
-        nulla posuere sollicitudin aliquam ultrices sagittis orci. Vitae proin
-        sagittis nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque
-        viverra mauris in aliquam sem fringilla ut. Nec nam aliquam sem et
-        tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a.
-        Elementum tempus egestas sed sed. Faucibus purus in massa tempor nec
-        feugiat nisl pretium fusce.
+        ac orci phasellus egestas tellus.
       </p>
-      <p>
-        Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc
-        mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Ut
-        faucibus pulvinar elementum integer enim. Odio facilisis mauris sit amet
-        massa vitae tortor condimentum lacinia. Eu non diam phasellus vestibulum
-        lorem sed risus. Porttitor rhoncus dolor purus non enim praesent. Sit
-        amet mauris commodo quis imperdiet. Lobortis feugiat vivamus at augue
-        eget. Nibh tellus molestie nunc non blandit. Tellus mauris a diam
-        maecenas sed enim ut. Tortor aliquam nulla facilisi cras fermentum odio
-        eu feugiat pretium. Mattis aliquam faucibus purus in massa.
-      </p>
+      
     </template>
 
     <template v-slot:footer>
       <div class="d-flex align-items-center justify-content-between">
-        <button
-          class="btn btn--secondary"
-          @click="$refs.modalName.closeModal()"
-        >
-          Cancel
-        </button>
-        <button class="btn btn--primary" @click="$refs.modalName.closeModal()">
-          Save
-        </button>
+        <p>Kanske någon mer info, typ författare, publicerings år, diarie nummer osv.</p>
       </div>
     </template>
   </modal>
