9eed3890bf0c7ea57f6639a51cfab4b1d55bd32f
ingridgrenholm <65853333+ingridgrenholm@users.noreply.github.com>
2021-05-26 17:01:25 +0200

    funkar

--- src/components/renderHtml.vue
+++ src/components/renderHtml.vue
@@ -1,53 +1,53 @@
 <template>
   <div>
-    <div v-html="html"></div>
+    <div v-html="html" v-on:click="handleClick()"></div>
   </div>
 </template>
 
 <script>
-
+import { ref } from "vue";
 export default {
-  setup() {
-    const html = require('../assets/html/sbk-avfallsplan.html');
-    let markedArray = []
+  emits: ["showCommentBox"],
+  setup(props, context) {
+    const html = require("../assets/html/sbk-avfallsplan.html");
+    let markedArray = [];
+    const showComment = ref(false);
+    const handleClick = () => {
+      context.emit("showCommentBox", showComment);
+    };
 
     window.onclick = function(event) {
-      
       if (markedArray.includes(event.target.className)) {
-        markedArray = markedArray.filter(classes => {
-          console.log(classes, 'classes')
-          return classes !== event.target.className
-        })
+        markedArray = markedArray.filter((classes) => {
+          console.log(classes, "classes");
+          return classes !== event.target.className;
+        });
       } else {
-        markedArray.push(event.target.className)
+        markedArray.push(event.target.className);
       }
 
       if (markedArray.includes(event.target.className)) {
-        event.target.style.background = "#AEFF14"
+        event.target.style.background = "#AEFF14";
       } else {
-        event.target.style.background = "transparent"
+        event.target.style.background = "transparent";
       }
-    }
+    };
 
-    return { html }
-  }
-}
+    return { html, showComment, handleClick };
+  },
+};
 </script>
 
-
 <style>
-  .iframe{
-    position: absolute;
-    top: 0;
-    height: 100%;
-    left: 0;
-    width: 100%;
-    z-index: -3;
-
-    
-  }
-  /* .t.m0.x7.hb.y13.ff1.fs7.fc2.sc0.ls0.ws0 {
+.iframe {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  left: 0;
+  width: 100%;
+  z-index: -3;
+}
+/* .t.m0.x7.hb.y13.ff1.fs7.fc2.sc0.ls0.ws0 {
     background-color: green!important;
   } */
-
 </style>
