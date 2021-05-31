548bb539947ba6f60bbc3f5a8f209a0f444bbf8c
electricGebring <56607795+electricGebring@users.noreply.github.com>
2021-05-31 12:20:38 +0200

    wip nu färgar vi elementet efter anrop

--- src/components/renderHtml.vue
+++ src/components/renderHtml.vue
@@ -5,51 +5,61 @@
 </template>
 
 <script>
-import { onMounted, watch } from '@vue/runtime-core';
+import { onMounted } from '@vue/runtime-core';
 import { useStore } from 'vuex'
 
 
 export default {
   setup() {
-    let commentData = store.state.documentComment
+    
     const store = useStore()
+    //let commentData = store.state.documentComment
     const html = require('../assets/html/sbk-avfallsplan.html');
     // here we gonna check with the stor instead
-    // const id = '60931cf40a9b83f4cc0e5266'
+    
     // const comment = 'Hej jag heter Johan och jag är snäll, nu så startar jag hotel! <br> Elefanter är grå och inte blå, var snäll och ändra det i texten'
 
-    onMounted(() => {
-      store.dispatch("getComments");
-      //console.log(store.state.documentComment, 'store.state.documentComment')
+    onMounted(async() => {
+      const id = '60931cf40a9b83f4cc0e5266'
+      await store.dispatch("getComments", id);
+      setTimeout(() => {
+        highlight()
+      }, 10)
     })
    
-    // const documentComment = computed(() => {
+    // const commentData = computed(() => {
     //  return store.state.documentComment
     // })
 
-    watch(store.state.documentComment, () => {
-      commentData = store.state.documentComment
-    })
+    // watch(commentData, () => {
+    //   console.log('watch')
+    //   highlight()
+    // })
+
+    
    
     window.onclick = function(event) {
-      // console.log(documentComment, 'documentComment')
-      
-      // See if clicked element is in array => push || filter
-      // console.log(documentComment, 'documentComment')
-      const classes = event.target.className
-      if (commentData.includes(classes)) {
-        store.dispatch("deleteDocumentComment", id, classes, comment)
-      } else {
-        store.dispatch("postDocumentComment", { id, classes, comment } )  
-      }
-      
-      // Highligth DOM element
-      if (commentData.includes(classes)) {
-        classes.style.background = "#AEFF14"
-      } else {
-        classes.style.background = "transparent"
-      }
-      
+      console.log(event.target.className)
+      event.target.style.background = "#AEFF14"
+      // const classes = event.target.className
+      // if (commentData.includes(classes)) {
+      //   store.dispatch("deleteDocumentComment", id, classes, comment)
+      // } else {
+      //   store.dispatch("postDocumentComment", { id, classes, comment } )  
+      // }
+    }
+
+    // Highligth DOM element
+    const highlight = () => {    
+      store.state.documentComment.forEach((element) => {
+          const elementToHighlight = document.getElementsByClassName(element.classes);
+          console.log(elementToHighlight, 'elementToHighlight')
+          elementToHighlight[0].style.background = "#AEFF14"
+         
+        // else {
+        //   element.classes.style.background = "transparent"
+        // }
+      })
     }
     return { html }
   }
